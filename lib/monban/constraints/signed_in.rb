7bc244c51d1b505c912b15e7239bafcc1c0c10d2
kbaba1001 <kbaba1001@users.noreply.github.com>
2015-10-29 19:06:13 +0900

    support scope in routes

--- lib/monban/constraints/signed_in.rb
+++ lib/monban/constraints/signed_in.rb
@@ -2,12 +2,16 @@ module Monban
   module Constraints
     # Rails route constraint for signed in users
     class SignedIn
+      def initialize(scope = Monban.config.session_default_scope)
+        @scope = scope
+      end
+
       # Checks to see if the constraint is matched by having a user signed in
       #
       # @param request [Rack::Request] A rack request
       def matches?(request)
         warden = request.env["warden"]
-        warden && warden.authenticated?
+        warden && warden.authenticated?(@scope)
       end
     end
   end
--- lib/monban/constraints/signed_out.rb
+++ lib/monban/constraints/signed_out.rb
@@ -2,12 +2,16 @@ module Monban
   module Constraints
     # Rails route constraint for signed out users
     class SignedOut
+      def initialize(scope = Monban.config.session_default_scope)
+        @scope = scope
+      end
+
       # Checks to see if the constraint is matched by not having a user signed in
       #
       # @param request [Rack::Request] A rack request
       def matches?(request)
         warden = request.env["warden"]
-        warden && warden.unauthenticated?
+        warden && warden.unauthenticated?(@scope)
       end
     end
   end
